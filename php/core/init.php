64af581a819b07e14c0ccc11cc4e42ec706fa23f
Maquiz1 <manquiz92@gmail.com>
2024-01-10 18:31:49 +0300

    fix 4

--- php/core/init.php
+++ php/core/init.php
@@ -2,25 +2,25 @@
 session_start();
 
 $GLOBALS['config'] = array(
-  'mysql' => array(
-        'host' => 'localhost',
+    'mysql' => array(
+        'host' => '127.0.0.1',
         'username' => 'root',
         'password' => '@Subaru@2022',
-      'db' => 'penplus'
-  	),
-  'remember' =>array(
-      'cookie_name' => 'hash',
-      'cookie_expiry' => '3600'
-  	),
-  'session' =>array (
-     'session_name' =>'user',
-     'token_name' => 'token',
-      'session_table' => 'tableName'
-  	 )
+        'db' => 'penplus'
+    ),
+    'remember' => array(
+        'cookie_name' => 'hash',
+        'cookie_expiry' => '3600'
+    ),
+    'session' => array(
+        'session_name' => 'user',
+        'token_name' => 'token',
+        'session_table' => 'tableName'
+    )
 );
 
-spl_autoload_register(function($class){
-	require_once 'php/classes/'.$class.'.php';
+spl_autoload_register(function ($class) {
+    require_once 'php/classes/' . $class . '.php';
 });
 date_default_timezone_set("Africa/Dar_es_Salaam");
 include_once 'php/functions/sanitize.php';
@@ -33,12 +33,12 @@ include_once "php/phpMailer/POP3.php";
 include_once "php/phpMailer/OAuth.php";
 
 
-if(Cookie::exists(config::get('remember/cookie_name')) && !Session::exists(config::get('session/session_name'))){
-$hash= Cookie::get(config::get('remember/cookie_name'));
-$hashCheck = DB::getInstance()->get('user_session',array('hash','=',$hash));
-if($hashCheck->count()){
+if (Cookie::exists(config::get('remember/cookie_name')) && !Session::exists(config::get('session/session_name'))) {
+    $hash = Cookie::get(config::get('remember/cookie_name'));
+    $hashCheck = DB::getInstance()->get('user_session', array('hash', '=', $hash));
+    if ($hashCheck->count()) {
 
- $user = new User($hashCheck->first()->user_id);
- $user->login();
- }
+        $user = new User($hashCheck->first()->user_id);
+        $user->login();
+    }
 }
